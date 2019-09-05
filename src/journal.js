cfb19714b60323bf01aae087a9676285f05062e4
Bahay Gulle Bilgi and Zubeyde Sisman <bahay@>
2019-09-05 13:20:51 -0700

    Business logic.

--- src/journal.js
+++ src/journal.js
@@ -1,36 +1,32 @@
-function Entry (title, body) {
+export function Entry (title, body) {
 this.title = title,
 this.body = body
 }
 
 Entry.prototype.wordCount = function() {
-  return this.title.split(" ").length + this.body.split(" ").length
+  return this.title.split(" ").length + this.body.split(" ").length;
 }
 
-Entry.prototype.vowelCount = function() {
+Entry.prototype.count = function() {
   const vowels = ["a", "e", "i", "o", "u"];
-  var outputVowels = [];
-  let counter = 0;
+  const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 
+  let outputVowels = [];
+  let outputConsonants = [];
 
-  for (let letter of this.body {
-    if (vowels.includes(letter)) {
-      counter ++
-      return === true,
-      outputVowels.push(vowels);
-      return outputVowels.length;
-    }
-    if (this.body[i].include(vowels)) {
+  let wholeArray = this.title.split('').concat(this.body.split(''));
 
+  for (let i = 0; i < wholeArray.length; i++) {
+    if (vowels.includes(wholeArray[i]) === true) {
+      outputVowels.push(wholeArray[i]);
+    } else if (consonants.includes(wholeArray[i]) === true) {
+      outputConsonants.push(wholeArray[i]);
     }
   }
+        return [outputVowels.length, outputConsonants.length];
 }
 
-Entry.prototype.consonants = function() {
-  var consonants = [];
-
-}
-Entry.prototype.getTeaser = function() {
-  var firstSentence = this.body.split("./?/!",1);
-  return firstSentence;
-}
+// Entry.prototype.getTeaser = function() {
+//   var firstSentence = this.body.split("./?/!",1);
+//   return firstSentence;
+// }
