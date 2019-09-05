9459965af7a417bc859e94a7445d365e0b57123d
Bahay Gulle Bilgi and Zubeyde Sisman <bahay@>
2019-09-05 13:20:19 -0700

    UI logic.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
+import { Entry } from './journal.js';
 import $ from 'jquery';
-import 'bootstraap';
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+
+    var inputtedTitle = $("input#title").val();
+    var inputtedBody = $("input#body").val();
+    var newEntry = new Entry (inputtedTitle, inputtedBody);
+     // newEntry.vowelCount();
+    $("#result").text(newEntry.count());
+  });
+});
